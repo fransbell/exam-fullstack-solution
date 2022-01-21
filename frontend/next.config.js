63b79b60544542b8df9eb24cbf156915730a3c11
fransbell <fransbirdsell@gmail.com>
2022-01-21 16:14:44 +0700

    dynamic test && build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
+const apiURI = process.env.DOCKER
+  ? "http://backend:1111/api/:path*"
+  : "http://localhost:1111/api/:path*"
+
+console.log(`apiuri : ${apiURI}`)
+
 module.exports = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:1111/api/:path*",
+        destination: apiURI,
       },
     ]
   },
